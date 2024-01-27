6ae46d857efa7688ba736f80a320b05c1cfe3163
Lucas Santi <idlucassanti@hotmail.com>
2024-01-27 01:58:13 -0300

    [ViewModel] Create ViewModel Start

---
+++ MVVM/ViewModels/StartViewModel.cs
@@ -0,0 +1,14 @@
+﻿using MVVM.Models;
+
+namespace MVVM.ViewModels
+{
+    public class StartViewModel
+    {
+        public Pearson Pearson { get; set; }
+
+        public StartViewModel()
+        {
+            Pearson = new Pearson();
+        }
+    }
+}
